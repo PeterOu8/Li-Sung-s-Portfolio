fda87f7abceaf986247d32cbe3c4d4b345cc1dfe
unknown <Peter1031734590@gmail.com>
2024-11-08 22:20:21 +1100

    refactor header

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -3,10 +3,10 @@ export const Header = () => {
         <header className="flex flex-row items-center justify-center py-3">
             <nav>
                 <ul className="flex gap-1 p-0.5 border-white/15 rounded-full bg-white/10 backdrop-blur">
-                    <a href="#" className="nav-item">Home</a>
-                    <a href="#" className="nav-item">Projects</a>
-                    <a href="#" className="nav-item">About</a>
-                    <a href="#" className="nav-item">Contact</a>
+                    <a href="/" className="nav-item">Home</a>
+                    <a href="/projects" className="nav-item">Projects</a>
+                    <a href="/about" className="nav-item">About</a>
+                    <a href="/contact" className="nav-item">Contact</a>
                 </ul>
             </nav>
         </header>
