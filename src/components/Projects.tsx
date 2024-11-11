0956250ec85840f6ea470e8c63d1e0eab95008bb
unknown <Peter1031734590@gmail.com>
2024-11-12 00:38:46 +1100

    fix project card squish

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -30,7 +30,7 @@ export default function ProjectSection({ limit }: Props) {
                         </Link>
                     </div>
                 )}
-                <div className="grid grid-cols-2 gap-4 sm:grid-cols-2">
+                <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
                     {projects.map((project, id) => (
                         <ProjectCard key={id} project={project} />
                     ))}
