9adbed88ecb37fb61ace5d5855ea2b42dc4f4d34
unknown <Peter1031734590@gmail.com>
2024-11-13 11:54:35 +1100

    change html text

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,7 @@
 export default function ContactPage() {
     return (
         <section className="flex flex-col items-center gap-10">
-            <h1 className="title text-5xl">Let&lsquo; work together</h1>
+            <h1 className="title text-5xl">Let&apos;s work together</h1>
         </section>
     );
 }
