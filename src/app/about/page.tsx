64bd92a2981ba002086b17076745eeccb93ec4e2
unknown <Peter1031734590@gmail.com>
2024-11-16 12:11:31 +1100

    add description on about me page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,15 @@ import { SkillIcons } from "@/components/SkillIcons";
 export default function AboutPage() {
     return (
         <section className="container flex flex-col gap-10">
-            <h1 className="title text-5xl">About me</h1>
+            <div className="flex flex-col gap-6">
+                <h1 className="title text-5xl">
+                            Hello, I am Li-Sung 👋
+                </h1>
+                <h2 className="text-foreground/80">
+                    I enjoy building scalable projects across all phases of a project, blending creativity with technical expertise to bring concepts to life.
+                </h2>
+            </div>
+            <h2 className="title">Tech Stack</h2>
             <SkillIcons />
         </section>
     );
